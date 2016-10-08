e88b68a0fa80302ac3ee36d11a01423f4352b4b2
Abu Hanife Nayem <ahnayem@student.sust.edu>
2016-10-08 18:34:00 +0600

    some documentation

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*Carousel view scripts*/
+$(document).ready(function(){
+
+    $("#testimonial").owlCarousel({
+        items: 1,
+        loop:true,
+        autoplay: true,
+        navText: ["<i class='fa fa-angle-right'></i>","<i class='fa fa-angle-left'></i>"],
+        center: true,
+        margin: 10,
+        nav: true,
+        autoHeight: true
+    });
+    /*Hover effect script for feature block*/
+    
+});
+
+$(document).ready(function(){
+        $(".feature-block").mouseover(function(){
+             $(this).addClass("onHover");
+        });
+        $(".feature-block").mouseout(function(){
+            $(this).removeClass("onHover");
+        });
+});
+
