a8329c46d44a449487f0402dc9d9b98ecec8019a
Abu Hanife Nayem <ahnayem@student.sust.edu>
2016-10-24 17:46:52 +0600

    sm

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
-/*Carousel view scripts*/
+
 $(document).ready(function(){
 
+    /*Carousel view scripts*/
     $("#testimonial").owlCarousel({
         items: 1,
         loop:true,
@@ -12,10 +13,10 @@ $(document).ready(function(){
         autoHeight: true
     });
 
+    /*Menu bar background color change script*/
     $("#menu-click").click(function(event){
             event.preventDefault();
         });
-    /*$("#nav").hide();*/
     $(window).scroll(function(){
        var scrollTop = $(window).scrollTop();
         if(scrollTop>100){
@@ -25,9 +26,24 @@ $(document).ready(function(){
         $("#nav").css("background-color", "transparent");
     }
     });
+
+    /*Smoth transition script*/
+    $('a[href*=#]:not([href=#])').click(function() {
+       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) 
+       {
+               var target = $(this.hash);
+               target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                   $('html,body').animate({
+                       scrollTop: target.offset().top
+                   }, 1000);
+               return false;
+           }
+       }
+    });
 });
 
-
+/*Nav bar script to open*/
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "20px";
@@ -36,10 +52,12 @@ function openNav() {
     document.getElementById("overlay").style.height = "100%";
     /*document.getElementById("overlay").style.backgroundColor = "rgba(0,0,0,0.4)";*/            
 }
-
+/*Script to close nav bar*/
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
     document.getElementById("overlay").style.width = "0%";
     document.getElementById("overlay").style.height = "0%";
 }
+
+
